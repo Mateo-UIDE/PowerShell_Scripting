f16f23700e1e55e921fbfe465c82559b18b7b994
adrianGS25 <chgarciasa@uide.edu.ec>
2025-12-12 15:02:00 -0500

    Add PowerShell progress bar script
    Agrego mi script del segundo entregable - Adrián García

---
+++ 02-BarraProgreso-AdrianGarcia.ps1
@@ -0,0 +1,20 @@
+############### NOMBRE Y APELLIDO DEL AUTOR(A): ADRIÁN GARCÍA ####################
+
+function Start-ProgressBar {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        $Title,
+        
+        [Parameter(Mandatory = $true)]
+        [int]$Timer
+    )
+    
+    for ($i = 1; $i -le $Timer; $i++) {
+        Start-Sleep -Seconds 1
+        $percentComplete = ($i / $Timer) * 100
+        Write-Progress -Activity $Title -Status "$i seconds elapsed" -PercentComplete $percentComplete
+    }
+}
+
+Start-ProgressBar -Title "Test timeout" -Timer 30
